0fbf999409c87f7e23cbab984e347d93710da1ec
hernang87 <hernang@mural.co>
2024-08-16 10:32:08 -0300

    Add fetch data useEffect

--- src/Loader/index.tsx
+++ src/Loader/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LoaderArgs } from "./types";
 
 export function Loader({ url }: LoaderArgs): JSX.Element {
-  console.log({ url });
+  const [text, setText] = React.useState<string | null>(null);
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  // UseMount
+  useEffect(() => {
+    setLoading(true);
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch(url, { signal })
+      .then((response) => response.text())
+      .then((text) => {
+        setText(text);
+        setLoading(false);
+      }).catch(() => setLoading(false));
+
+    return () => controller.abort();
+  }, [url]);
+
   return <div>Loading...</div>;
 }
